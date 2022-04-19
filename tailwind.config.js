f84cc98f306bce6f750b6f2d4148dd431eb31004
Emmanuel López <emmlop243@gmail.com>
2022-04-19 11:03:29 -0500

    Tailwind color and font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#058179',
+        primaryLight: '#E2EEEC',
+        greyLight: '#F7F7F7',
+        greyMedium: '#E6E6E6',
+        grey: '#5E666E',
+        success: '#61C255',
+        warning: '#F9DB72',
+        needed: '#EA9E2C',
+        danger: '#E35656',
+      },
+    },
   },
   plugins: [],
 };
